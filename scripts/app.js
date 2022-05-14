6e19248ceec1af39b24dbf92a9f48808460c992c
TheSowut <sowutmail@gmail.com>
2022-05-14 21:56:09 +0300

    feat: reset preferences button

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,7 @@
+const localStorageItem = 'background-color';
+const defaultBackgroundColor = "#232627";
+const defaultTextColor = "#ffffff";
+
 window.onload = () => {
     addSubscriptions();
     setSavedPreferences();
@@ -7,12 +11,13 @@ window.onload = () => {
  * Add Event Listener for click event on buttons.
  */
 const addSubscriptions = () => {
-    const buttons = ["primary-button", "secondary-button", "accent-button"];
+    const buttons = ['primary-button', 'secondary-button', 'accent-button'];
     buttons.map(button => {
         const el = document.querySelector(`#${button}`);
-        el.addEventListener("click", setColor);
+        el.addEventListener('click', setColor);
         el.color = window.getComputedStyle(document.documentElement).getPropertyValue(`--${button}`);
     });
+    document.querySelector('#reset-button').addEventListener('click', resetPreferences);
 }
 
 /**
@@ -20,22 +25,31 @@ const addSubscriptions = () => {
  * @param event
  */
 const setColor = (event) => {
-    document.documentElement.style.setProperty('--bckg', `hsl(${event.currentTarget.color}, 100%, 50%)`);
-    document.querySelector(":root").style.setProperty('--text-color', `hsl(${event.currentTarget.color - 180}, 100%, 50%)`);
-    localStorage.setItem('background-color', event.currentTarget.color);
+    document.querySelector(':root').style.setProperty('--bckg', `hsl(${event.currentTarget.color}, 100%, 50%)`);
+    document.querySelector(':root').style.setProperty('--text-color', `hsl(${event.currentTarget.color - 180}, 100%, 50%)`);
+    localStorage.setItem(localStorageItem, event.currentTarget.color);
 }
 
 /**
  * Used to extract saved preferences from local storage and set theme.
  */
 const setSavedPreferences = () => {
-    if (localStorage.getItem('background-color')) {
+    if (localStorage.getItem(localStorageItem)) {
         // Create an object with event like structure so we can reuse the setColor function.
         const preferredColor = {
             currentTarget: {
-                color: localStorage.getItem('background-color')
+                color: localStorage.getItem(localStorageItem)
             }
         };
         setColor(preferredColor);
     }
+}
+
+/**
+ * Used to remove saved preferences from local storage and reset theme.
+ */
+const resetPreferences = () => {
+    localStorage.removeItem(localStorageItem);
+    document.querySelector(':root').style.setProperty('--bckg', `hsl(195, 5%, 15%)`);
+    document.querySelector(':root').style.setProperty('--text-color', `hsl(0, 0%, 100%)`);
 }
